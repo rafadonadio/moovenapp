781bf7b78fb8854437a92e2bb3761647b6b3cb85
Guillermo <guillefd@gmail.com>
2017-10-10 00:07:24 -0300

    home refactor + cards

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,9 +1,14 @@
+import { HelpPage } from '../help/help';
+import { SettingsPage } from '../settings/settings';
 import { ShipmentsTabsPage } from '../shipments-tabs/shipments-tabs';
 import { SendingsTabsPage } from '../sendings-tabs/sendings-tabs';
+import { AccountService } from '../../providers/account-service/account-service';
 import { SendingService } from '../../providers/sending-service/sending-service';
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { APP_CFG } from '../../models/app-model';
+import { Subscription } from 'rxjs/Rx';
+import { UserAccount, UserAccountOperator } from '../../models/user-model';
 
 @Component({
     selector: 'page-home',
@@ -11,27 +16,65 @@ import { APP_CFG } from '../../models/app-model';
 })
 export class HomePage {
 
+    // operatorAuth flags
+    operatorAuthUnchecked:any;
+    // account
+    accountSubs: Subscription;
+    account: UserAccount;
+    operator: UserAccountOperator;
+    // 
     appName:string = APP_CFG.ENVIRONMENTS[APP_CFG.CURRENT_ENV].APP_NAME;
     sendings:any;
     shipments:any;
 
     constructor(public navCtrl: NavController, 
         public navParams: NavParams,
-        private sendingSrv: SendingService) {
+        private sendingSrv: SendingService,
+        private accountSrv: AccountService) {
     }
 
-    ionViewWillLoad() {
-        console.log('ionViewDidLoad HomePage');
-        this.getActiveSendings();
-        this.getActiveShipments();
+    ionViewWillEnter() {
+        this.operatorAuthUnchecked = true;
+        this.init();  
     }
+
+    private init() {
+        this.getActiveSendings();        
+        this.setAccount()
+            .then(() => {
+                this.getActiveShipments();
+            });
+    }    
+
+    private setAccount(): Promise<any> {
+        return new Promise((resolve, reject) => {
+            let obs = this.accountSrv.getObs(true);
+            this.accountSubs = obs.subscribe((snap) => {
+                this.account = snap.val();
+                this.operator = this.account.operator;
+                this.operatorAuthUnchecked = false;
+                console.log('set account', this.account, this.operator, this.operatorAuthUnchecked);
+                resolve();
+            },
+            err => {
+                console.log(err);
+                reject();
+            });
+        });
+    }    
 
     goToSendings() {
         this.navCtrl.setRoot(SendingsTabsPage);
     }
     goToShipments() {
         this.navCtrl.setRoot(ShipmentsTabsPage);
-    }    
+    } 
+    goToHelp() {
+        this.navCtrl.setRoot(HelpPage);
+    } 
+    goToSettings() {
+        this.navCtrl.setRoot(SettingsPage);
+    }            
 
     private getActiveSendings() {
         this.sendings = this.sendingSrv.getAllActiveObs();
