c3298cd9d200868d282f416e456f35298edd0018
Guillermo <guillefd@gmail.com>
2017-11-16 12:05:30 -0300

    Revert "env not working"
    This reverts commit ef08d0a1eb820043020a9d024b728a83702266ab.

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,19 +1,31 @@
+var chalk = require("chalk");
+var fs = require('fs');
 var path = require('path');
-var defaultConfig = require('@ionic/app-scripts/config/webpack.config.js');
+var useDefaultConfig = require('@ionic/app-scripts/config/webpack.config.js');
 
 var env = process.env.IONIC_ENV;
 
-var devWebPackConfig = defaultConfig.dev;
-devWebPackConfig.resolve.alias = {
-  "@app/env": path.resolve('./src/environments/environment.' + env + '.ts')
-};
+if (env === 'prod' || env === 'dev') {
+    useDefaultConfig[env].resolve.alias = {
+        "@app/env": path.resolve(environmentPath())
+    };
+} else {
+    // Default to dev config
+    useDefaultConfig[env] = useDefaultConfig.dev;
+    useDefaultConfig[env].resolve.alias = {
+        "@app/env": path.resolve(environmentPath())
+    };
+}
 
-var prodWebPackConfig = defaultConfig.prod;
-prodWebPackConfig.resolve.alias = {
-  "@app/env": path.resolve('./src/environments/environment.' + env + '.ts')
-};
+function environmentPath() {
+    var filePath = './src/environments/environment' + (env === 'prod' ? '' : '.' + env) + '.ts';
+    if (!fs.existsSync(filePath)) {
+        console.log(chalk.red('\n' + filePath + ' does not exist!'));
+    } else {
+        return filePath;
+    }
+}
 
-module.exports = {
-  dev: devWebPackConfig,
-  prod: prodWebPackConfig
+module.exports = function() {
+    return useDefaultConfig;
 };
