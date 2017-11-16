ef08d0a1eb820043020a9d024b728a83702266ab
Guillermo <guillefd@gmail.com>
2017-11-16 12:05:11 -0300

    env not working

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,31 +1,19 @@
-var chalk = require("chalk");
-var fs = require('fs');
 var path = require('path');
-var useDefaultConfig = require('@ionic/app-scripts/config/webpack.config.js');
+var defaultConfig = require('@ionic/app-scripts/config/webpack.config.js');
 
 var env = process.env.IONIC_ENV;
 
-if (env === 'prod' || env === 'dev') {
-    useDefaultConfig[env].resolve.alias = {
-        "@app/env": path.resolve(environmentPath())
-    };
-} else {
-    // Default to dev config
-    useDefaultConfig[env] = useDefaultConfig.dev;
-    useDefaultConfig[env].resolve.alias = {
-        "@app/env": path.resolve(environmentPath())
-    };
-}
+var devWebPackConfig = defaultConfig.dev;
+devWebPackConfig.resolve.alias = {
+  "@app/env": path.resolve('./src/environments/environment.' + env + '.ts')
+};
 
-function environmentPath() {
-    var filePath = './src/environments/environment' + (env === 'prod' ? '' : '.' + env) + '.ts';
-    if (!fs.existsSync(filePath)) {
-        console.log(chalk.red('\n' + filePath + ' does not exist!'));
-    } else {
-        return filePath;
-    }
-}
+var prodWebPackConfig = defaultConfig.prod;
+prodWebPackConfig.resolve.alias = {
+  "@app/env": path.resolve('./src/environments/environment.' + env + '.ts')
+};
 
-module.exports = function() {
-    return useDefaultConfig;
+module.exports = {
+  dev: devWebPackConfig,
+  prod: prodWebPackConfig
 };
